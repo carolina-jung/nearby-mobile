32711931b5b98641a6c0f4d2c9acc17f2d531d9e
Carolina Jung do Amaral <carolina.amaral@pmpa.ad>
2024-12-15 16:31:45 -0300

    Adicionado permissão de CORS para todas as origens para usar em ambiente dev

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,14 +1,16 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
 
-import { routes } from "./routes"
-import { errorHandling } from "./middlewares/error-handling"
+import { routes } from "./routes";
+import { errorHandling } from "./middlewares/error-handling";
 
-const PORT = 3333
-const app = express()
+const PORT = 3333;
+const app = express();
 
-app.use(express.json())
-app.use(routes)
+app.use(cors()); // Permite CORS para todas as origens, usar apenas em ambiente dev
+app.use(express.json());
+app.use(routes);
 
-app.use(errorHandling)
+app.use(errorHandling);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
